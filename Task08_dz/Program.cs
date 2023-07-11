7dc89ee218bf4d2ebd6b986b5faf98194e85d4f4
«pavel <pavel.zacepin@list.ru>
2023-07-11 16:21:40 +0300

    dz - task08

---
+++ Task08_dz/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 1)
+{
+    Console.WriteLine("Отсутствуют четные числа");
+}
+if (n <= 0)
+{
+    Console.WriteLine("Ошибка ввода");
+}
+else
+{
+    int counter = 2;
+    Console.Write("Четные числа: ");
+    while (counter <= n)
+
+    {
+        Console.Write(counter + " ");
+        counter = counter + 2;
+    }
+}
